71af0879425536bd47ab5cfebab0df708f9f3097
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-25 19:46:34 +0500

    Add files via upload

---
+++ user 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e,f,g,h;
+	cin>>a>>b>>c>>d>>e>>f>>g>>h;
+cout<<"a"<<" "<<"b"<<" "<<"c"<<" "<<"d"<<endl;
+cout<<"e"<<" "<<"f"<<" "<<"g"<<" "<<"h"<<endl;	
+}
